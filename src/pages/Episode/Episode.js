252a4eba31b25b0f0996154bc23130532b219ad5
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-05-29 11:15:43 +0300

    fix episode rendering

--- src/pages/Episode/Episode.js
+++ src/pages/Episode/Episode.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ShowDetails from '../../components/ShowDetails/ShowDetails';
 
-const Episode = ({ episode }) => (
-  <div className="episode_container">
-    <div className="episode_container__homelink">
-      <Link to="/">Back To Show Page</Link>
-    </div>
-    <ShowDetails {...episode} />
-  </div>
-);
+class Episode extends Component {
+  componentDidMount() {
+    if (!this.props.episode.id) this.props.fetchEpisodes();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.episodes !== this.props.episodes) {
+      const choosedEpisode = this.props.episodes.find(episode =>
+        episode.id === +this.props.match.params.episode);
+      this.props.setEpisode(choosedEpisode);
+    }
+  }
+
+  render() {
+    const { episode } = this.props;
+    return (
+      <div className="episode_container">
+        <div className="episode_container__homelink">
+          <Link to="/">Back To Show Page</Link>
+        </div>
+        <ShowDetails {...episode} />
+      </div>
+    );
+  }
+}
 
 Episode.propTypes = {
   episode: PropTypes.object.isRequired,
--- src/pages/Episode/EpisodeContainer.js
+++ src/pages/Episode/EpisodeContainer.js
@@ -1,8 +1,16 @@
 import { connect } from 'react-redux';
+import { setEpisode } from '../../actions/episode';
+import { fetchEpisodes } from '../../actions/episodes';
 import Episode from './Episode';
 
 const mapStateToProps = state => ({
   episode: state.episode,
+  episodes: state.episodes,
 });
 
-export default connect(mapStateToProps)(Episode);
+const mapDispatchToProps = dispatch => ({
+  fetchEpisodes: () => dispatch(fetchEpisodes()),
+  setEpisode: episode => dispatch(setEpisode(episode)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Episode);
